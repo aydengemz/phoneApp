05f8ca7536f173366bff664f099566f86f21f35c
Ayden Xu <aydenxunc@gmail.com>
2018-11-18 09:49:41 -0500

    Initial Commit

---
+++ phoneApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  phoneApp
+//
+//  Created by Ayden Xu on 11/18/18.
+//  Copyright © 2018 Ayden Xu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
